ecd5c9f21059fd44056c385a31cd0f91a74178bb
ywang943 <ywang943@connect.hkust-gz.edu.cn>
2026-04-10 16:42:45 +0800

    Update eval_repo.py

--- src/eval_repo.py
+++ src/eval_repo.py
@@ -78,7 +78,7 @@ REMOTE_API_KEY = os.getenv(
     "REMOTE_API_KEY",
     os.getenv(
         "GPT_API_KEY",
-        "7bc70a62f11a48c18b00284cac02a7305753c7e4cce748bdb6d80791cfd32459",
+        "",
     ),
 )
 GPT_API_URL = os.getenv(
